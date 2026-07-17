1bea2b600b7e5313c255ac1460890480004e7528
Gavin <gavin.henkwamdee@gmail.com>
2026-07-17 11:37:26 +0700

    feat: add GA4 tracking tag G-P8FCWRLNZ7

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-P8FCWRLNZ7" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-P8FCWRLNZ7');
+            `,
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
